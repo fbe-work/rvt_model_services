1b32589ec40d845500a5584a29e1bdb635b7ef83
f.beaupere <dt12345/>
2017-06-13 11:46:42 +0200

    warnings - refactored from warn to match command name

--- commands/warn/__init__.py
+++
@@ -1,4 +0,0 @@
-register = {"name": "qc",
-            "get_rps_button": "qc_model",
-            "optional_html_path": True,
-            }
---
+++ commands/warnings/__init__.py
@@ -0,0 +1,4 @@
+register = {"name": "warnings",
+            "rvt_journal_writer": "warnings_export_command",
+            "optional_html_path": True,
+            }
