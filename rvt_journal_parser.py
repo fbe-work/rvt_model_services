cb84dfaaf2a9043df4831d382ec874abbc527706
hdm-dt-fb <dt12345/>
2017-06-29 17:51:33 +0200

    rvt_journal_parser - search key more generic

--- rvt_journal_parser.py
+++ rvt_journal_parser.py
@@ -4,7 +4,9 @@ rvt_journal_parser
 find specific messages in a given journal file
 to detect model corruption, missing links and the like.
 """
-model_corrupt = 'CorruptElemStream'
+import os.path as op
+
+model_corrupt = 'orrupt'
 missing_links = 'TaskDialog "Revit could not find or read'
 key_phrases = {missing_links: "missing_links",
                model_corrupt: "corrupt",
@@ -19,16 +21,17 @@ def read_journal(journal_path):
     """
     detected = {}
     with open(journal_path, 'rb') as journal:
+        journal_name = op.basename(journal_path)
         for line in journal:
             decoded_line = line.decode("latin1", "ignore")
             if model_corrupt in decoded_line:
                 print("!!_Corrupt_Model_!!")
                 print(journal_path)
                 print(decoded_line)
-                detected[journal_path] = key_phrases[model_corrupt]
+                detected[journal_name] = key_phrases[model_corrupt]
             if missing_links in decoded_line:
                 print("!!_Missing_Links_!!")
                 print(journal_path)
                 print(decoded_line)
-                detected[journal_path] = key_phrases[missing_links]
+                detected[journal_name] = key_phrases[missing_links]
     return detected
