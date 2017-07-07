63c6d692b6cda10ed1a35059919a3ed5b4064e87
hdm-dt-fb <dt12345/>
2017-07-07 15:34:54 +0200

    send_mail - mail notifier added

---
+++ notify/email/send_mail.py
@@ -0,0 +1,24 @@
+import configparser
+import smtplib
+from email.mime.text import MIMEText
+
+
+def fetch_config(prj_number):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    server = config[prj_number]["server"]
+    sender = config[prj_number]["sender"]
+    receiver = config[prj_number]["receiver"]
+    return server, sender, receiver
+
+
+def notify(prj_number, prj_path, port=25):
+    mail_server, mail_sender, mail_recipients = fetch_config(prj_number)
+    mail_text = f"warning - rvt model {prj_number} at path {prj_path} is corrupt!"
+    msg = MIMEText(mail_text)
+    msg['From'] = mail_sender
+    msg['To'] = mail_recipients
+    msg['Subject'] = f"{prj_number} - rvt model corrupt"
+
+    with smtplib.SMTP(f"{mail_server}:{port}") as smtp_con:
+        smtp_con.send_message(msg)
