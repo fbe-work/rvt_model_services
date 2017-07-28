f22f2bc1f9c4ba6bfb7c977075c00c64e3b71abf
hdm-dt-fb <dt12345/>
2017-07-28 17:36:58 +0200

    qc/__init__.py - register post_process added

--- commands/qc/__init__.py
+++ commands/qc/__init__.py
@@ -1,4 +1,8 @@
+from . import bokeh_qc_graphs
+
 register = {"name": "qc",
             "get_rps_button": "qc_model",
             "optional_html_path": True,
+            "post_process": {"func": bokeh_qc_graphs.update_graphs,
+                             "args": ["project_code", "html_path"]},
             }
