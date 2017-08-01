aeca3da5e82adcd024fcb54f8d3e5f5244feffed
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-08-01 14:24:27 -0700

    Fixed syntax for IronPython
    Revit runs ipy2.7 not py3.7

--- commands/cookiecutter/cookie.py
+++ commands/cookiecutter/cookie.py
@@ -1,8 +1,9 @@
 """
-this is just a template to shot that your command module can run python code as post process
+This is just a template to show that your command module
+can run python code as post process.
 """
 
 
 def says_hi(project_code):
-    print(f"Hi there, I am your {project_code} post_process cookie.py message!\n"
-          f"Thank you for using rvt_model_services!!")
+    print("Hi there, I am your {} post_process cookie.py message!\n"
+          "Thank you for using rvt_model_services!!".format(project_code))
