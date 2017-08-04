4641fee37279abfc0cefaf236c127fac7c01fc19
hdm-dt-fb <dt12345/>
2017-08-04 17:46:10 +0200

    rvt_journal_writer - jrn compose blocks added

--- rvt_journal_writer.py
+++ rvt_journal_writer.py
@@ -5,6 +5,16 @@ rvt_journal_writer
 import rps_xml
 
 
+def build_jrn(**kwargs):
+    """
+    Composes a journal file of journal blocks.
+    :param kwargs: journal blocks
+    :return:
+    """
+    jrn_text = "\n".join(kwargs.values())
+    return jrn_text
+
+
 def write_journal(journal_file_path, journal_template, full_model_path, command):
     """
     Writes a journal to be used with revit, based on
@@ -97,6 +107,40 @@ rps_addin_template = """<?xml version="1.0" encoding="utf-16" standalone="no"?>
 """
 
 audit = ''
+
+jrn_blocks = {
+"header":
+"""' 0:< 'C 27-Oct-2016 19:33:31.459;
+Dim Jrn
+Set Jrn = CrsJournalScript
+""",
+
+"open_dialogue":
+""" Jrn.Command "Internal" , "Show or hide recent files , ID_STARTUP_PAGE"
+ Jrn.Command "Internal" , "Open an existing project , ID_REVIT_FILE_OPEN"
+""",
+
+"ws_detach":
+""" Jrn.Data "FileOpenSubDialog" , "OpenAsLocalCheckBox", "True"
+ Jrn.Data "FileOpenSubDialog" , "DetachCheckBox", "True"
+ Jrn.Data "FileOpenSubDialog" , "OpenAsLocalCheckBox", "False"
+ Jrn.Data "TaskDialogResult" , "Detaching this model will create an independent model. You will be unable to synchronize your changes with the original central model." & vbLf & "What do you want to do?", "Detach and preserve worksets", "1001"
+""",
+
+"audit":
+""" Jrn.Data "FileOpenSubDialog"  , "AuditCheckBox", "True"
+ Jrn.Data "TaskDialogResult" , "This operation can take a long time. Recommended use includes periodic maintenance of large files and preparation for upgrading to a new release. Do you want to continue?",  "Yes", "IDYES"
+""",
+
+"open":
+""" Jrn.Data "File Name" , "IDOK",{0}
+""",
+
+"close_no_save":
+""" Jrn.Command "SystemMenu" , "Quit the application; prompts to save projects , ID_APP_EXIT"
+ Jrn.Data "TaskDialogResult" , "Do you want to save changes to Untitled?", "No", "IDNO"
+"""
+}
 
 if __name__ == "__main__":
     rvt_path = "N:/456_Roche-Bau-8-und-11/02_DRAWINGS/3_BASIC_DESIGN/1_OFFICIAL/BIM/01_CENTRAL/"
