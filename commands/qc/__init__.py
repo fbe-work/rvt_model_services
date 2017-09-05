b729826a992b5cdfffb103bb8d6347e76d1a9c70
hdm-dt-fb <dt12345/>
2017-09-05 09:25:54 +0200

    qc init - cleanup

--- commands/qc/__init__.py
+++ commands/qc/__init__.py
@@ -6,18 +6,14 @@ from . import bokeh_qc_graphs
 def cmd_journal(project_code, model_path, jrn_path, com_dir, log_dir):
     com_qc_dir = op.join(com_dir, op.basename(op.dirname(__file__)))
     rvt_jrn = rjm.JournalMaker(permissive=False)
-    rvt_jrn.open_workshared_model(model_path=model_path,
-                                  detached=True,
-                                  audit=True
-                                  )
+    rvt_jrn.open_workshared_model(model_path=model_path, detached=True, audit=True)
     com_data = {"SearchPaths": com_qc_dir,
                 "ModelName": op.basename(model_path),
                 "OutputPath": log_dir,
-                "OutputPrefix": "Session_prefix_",
+                "OutputPrefix": project_code,
                 "LogFile": op.join(log_dir, "rms_exec_results.log"),
                 "ScriptSource": op.join(com_qc_dir, "rps_qc_model.py"),
                 }
-
     rvt_jrn.execute_command(tab_name='Add-Ins',
                             panel_name='  Revit Model Services (RMS)  ',
                             command_module='RMSCmdExecutor',
@@ -29,8 +25,6 @@ def cmd_journal(project_code, model_path, jrn_path, com_dir, log_dir):
 
 
 register = {"name": "qc",
-            "get_rps_button": "qc_model",
-            "optional_html_path": True,
             "post_process": {"func": bokeh_qc_graphs.update_graphs,
                              "args": ["project_code", "html_path"]},
             "rjm": cmd_journal,
