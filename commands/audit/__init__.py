668675c7988aa223d63040071653fd9074facf09
hdm-dt-fb <dt12345/>
2017-09-12 12:09:45 +0200

    audit init - non-permissive add

--- commands/audit/__init__.py
+++ commands/audit/__init__.py
@@ -2,7 +2,7 @@ import rjm
 
 
 def cmd_journal(project_code, model_path, jrn_path, com_dir, log_dir):
-    rvt_jrn = rjm.JournalMaker()
+    rvt_jrn = rjm.JournalMaker(permissive=False)
     rvt_jrn.open_workshared_model(model_path=model_path, detached=True, audit=True)
     rvt_jrn.close_model()
     rvt_jrn.write_journal(jrn_path)
