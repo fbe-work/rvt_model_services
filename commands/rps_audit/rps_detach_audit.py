a3b9a364fbfd2f652e55e36b5a42ec6c208aea23
hdm-dt-fb <dt12345/>
2018-01-29 12:00:36 +0100

    rps_detach_audit - fam doc error trap add

--- commands/rps_audit/rps_detach_audit.py
+++ commands/rps_audit/rps_detach_audit.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import clr
 clr.AddReference("RevitAPI")
 from Autodesk.Revit.DB import OpenOptions, DetachFromCentralOption, FilePath
@@ -16,5 +17,10 @@ else:
     model_path = os.environ["RVT_QC_PATH"]
 
     rvt_path = FilePath(model_path)
-    app.OpenDocumentFile(rvt_path, open_opt)
-    # print("{} - {}".format(project, model_path))
+    try:
+        app.OpenDocumentFile(rvt_path, open_opt)
+        # print("{} - {}".format(project, model_path))
+    except:
+        # added for models with a specific familiy corruption
+        print("loading model {} failed".format(rvt_path))
+        sys.exit()
