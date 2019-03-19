cd1ef4b0f51770989531bdc4a81fc544cfd83845
hdm-dt-fb <dt12345/>
2019-03-19 15:35:41 +0100

    fix error handling

--- notify/slack/send_slack.py
+++ notify/slack/send_slack.py
@@ -8,7 +8,7 @@ http://slackapi.github.io/python-slackclient/
 Usage:
 from notify.slack import send_slack
 
-send_slack.notify(project_code, "Your message here")
+send_slack.notify(project_code, project_path, "Your message here")
 """
 
 from slackclient import SlackClient
@@ -24,23 +24,21 @@ def fetch_config(prj_number):
         if prj_number in config:
             token = config[prj_number]["token"]
             channel = config[prj_number]["channel"]
-            return token, channel 
-    else:
-        return None, None
+            return token, channel
+    return None, None
 
 
 def notify(prj_number, prj_path, msg):
     slack_token, slack_channel = fetch_config(prj_number)
+    if slack_token and slack_channel:
+        sc = SlackClient(slack_token)
+        notify_text = f"warning - rvt model {prj_number} at path {prj_path} is corrupt! \nsee journal: {msg}"
 
-    sc = SlackClient(slack_token)
-
-    notify_text = f"warning - rvt model {prj_number} at path {prj_path} is corrupt! \nsee journal: {msg}"
-    
-    sc.api_call(
-                "chat.postMessage",
-                channel=slack_channel,
-                text=notify_text,
-                username="rvt_model_services",
-                # placeholder icon
-                icon_url="http://lorempixel.com/48/48/"
-                )
+        sc.api_call(
+                    "chat.postMessage",
+                    channel=slack_channel,
+                    text=notify_text,
+                    username="rvt_model_services",
+                    # placeholder icon
+                    icon_url="http://lorempixel.com/48/48/"
+                    )
