66bdbc74d627db3c127a83918d522c20de776302
hdm-dt-fb <dt12345/>
2020-04-14 16:41:46 +0200

    add attachment sending

--- notify/email/send_mail.py
+++ notify/email/send_mail.py
@@ -2,6 +2,8 @@ import os.path as op
 import configparser
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 
 def fetch_config(prj_number):
@@ -14,19 +16,30 @@ def fetch_config(prj_number):
             sender = config[prj_number]["sender"]
             receiver = config[prj_number]["receiver"]
             return server, sender, receiver
+    print("no mail config.ini found")
     return None, None, None
 
 
-def notify(prj_number, prj_path, journal_excerpt, subject="", port=25):
+def notify(prj_number, prj_path, journal_excerpt, subject="", attachment="", port=25):
     mail_server, mail_sender, mail_recipients = fetch_config(prj_number)
     if not subject:
         subject = "rvt model corrupt!!"
     if mail_server and  mail_sender and mail_recipients:
         mail_text = f"warning - rvt model {prj_number} at path {prj_path} : {subject}! \nsee: {journal_excerpt}"
-        msg = MIMEText(mail_text)
+        msg = MIMEMultipart()
+        msg.attach(MIMEText(mail_text))
         msg['From'] = mail_sender
         msg['To'] = mail_recipients
         msg['Subject'] = f"{prj_number} - {subject}"
+
+        if attachment:
+            with open(attachment, "rb") as csv_file:
+                part = MIMEApplication(
+                    csv_file.read(),
+                    Name=op.basename(attachment),
+                )
+                part['Content-Disposition'] = f'attachment; filename="{op.basename(attachment)}"'
+                msg.attach(part)
 
         with smtplib.SMTP(f"{mail_server}:{port}") as smtp_con:
             smtp_con.send_message(msg)
