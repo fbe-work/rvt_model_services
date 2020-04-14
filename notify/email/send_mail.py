2c50e99f0892638a825e163a0b902c7f8c939df3
hdm-dt-fb <dt12345/>
2020-04-14 14:21:47 +0200

    adjust notify to be more general

--- notify/email/send_mail.py
+++ notify/email/send_mail.py
@@ -17,14 +17,16 @@ def fetch_config(prj_number):
     return None, None, None
 
 
-def notify(prj_number, prj_path, journal_excerpt, port=25):
+def notify(prj_number, prj_path, journal_excerpt, subject="", text="", port=25):
     mail_server, mail_sender, mail_recipients = fetch_config(prj_number)
+    if not subject:
+        subject = "rvt model corrupt!!"
     if mail_server and  mail_sender and mail_recipients:
-        mail_text = f"warning - rvt model {prj_number} at path {prj_path} is corrupt! \nsee journal: {journal_excerpt}"
+        mail_text = f"warning - rvt model {prj_number} at path {prj_path} : {subject}! \nsee: {journal_excerpt}"
         msg = MIMEText(mail_text)
         msg['From'] = mail_sender
         msg['To'] = mail_recipients
-        msg['Subject'] = f"{prj_number} - rvt model corrupt!!"
+        msg['Subject'] = f"{prj_number} - {subject}"
 
         with smtplib.SMTP(f"{mail_server}:{port}") as smtp_con:
             smtp_con.send_message(msg)
