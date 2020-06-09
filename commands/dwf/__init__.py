ff8327888d40ea13deb058f4394b145272b436c2
hdm-dt-fb <dt12345/>
2020-06-09 09:53:18 +0200

    add time stamp and command to rms_exec log

--- commands/dwf/__init__.py
+++ commands/dwf/__init__.py
@@ -1,28 +1,34 @@
+import datetime
 import rjm
-import os.path as op
+from pathlib import Path
 
 
 def cmd_journal(project_code, model_path, jrn_path, com_dir, log_dir):
-    com_dwf_dir = op.join(com_dir, op.basename(op.dirname(__file__)))
+    command_path = Path(__file__).parent
+    command_name = command_path.name
+    time_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     rvt_jrn = rjm.JournalMaker(permissive=False)
     rvt_jrn.open_workshared_model(model_path=model_path, detached=True, audit=True)
-    com_data = {"SearchPaths": com_dwf_dir,
-                "ModelName": op.basename(model_path),
-                "OutputPath": log_dir,
-                "OutputPrefix": project_code,
-                "LogFile": op.join(log_dir, "rms_exec_results.log"),
-                "ScriptSource": op.join(com_dwf_dir, "rps_print_dwf_set.py"),
-                }
-    rvt_jrn.execute_command(tab_name='Add-Ins',
-                            panel_name='  Revit Model Services (RMS)  ',
-                            command_module='RMSCmdExecutor',
-                            command_class='RMSCmdExecutorCommand',
-                            command_data=com_data,
-                            )
+    com_data = {
+        "SearchPaths": command_path,
+        "ModelName": model_path.name,
+        "OutputPath": log_dir,
+        "OutputPrefix": project_code,
+        "LogFile": log_dir / f"{time_stamp}_{command_name}_rms_exec_results.log",
+        "ScriptSource": command_path / "rps_print_dwf_set.py",
+    }
+    rvt_jrn.execute_command(
+        tab_name='Add-Ins',
+        panel_name='  Revit Model Services (RMS)  ',
+        command_module='RMSCmdExecutor',
+        command_class='RMSCmdExecutorCommand',
+        command_data=com_data,
+    )
     rvt_jrn.close_model()
     rvt_jrn.write_journal(jrn_path)
 
 
-register = {"name": "dwf",
-            "rjm": cmd_journal,
-            }
+register = {
+    "name": "dwf",
+    "rjm": cmd_journal,
+}
