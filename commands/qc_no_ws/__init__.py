67d0d91f3341cdd6f8db3914fb20a2182d6d35b4
hdm-dt-fb <dt12345/>
2020-06-09 10:01:39 +0200

    add time stamp and command to rms_exec log

--- commands/qc_no_ws/__init__.py
+++ commands/qc_no_ws/__init__.py
@@ -1,31 +1,37 @@
+import datetime
 import rjm
-import os.path as op
+from pathlib import Path
 from . import bokeh_qc_graphs
 
 
 def cmd_journal(project_code, model_path, jrn_path, com_dir, log_dir):
-    op.basename(op.dirname(__file__))
-    com_qc_dir = op.join(com_dir, op.basename(op.dirname(__file__)))
+    command_path = Path(__file__).parent
+    command_name = command_path.name
+    time_stamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     rvt_jrn = rjm.JournalMaker(permissive=False)
-    com_data = {"SearchPaths": com_qc_dir,
-                "ModelName": op.basename(model_path),
-                "OutputPath": log_dir,
-                "OutputPrefix": project_code,
-                "LogFile": op.join(log_dir, "rms_exec_results.log"),
-                "ScriptSource": op.join(com_qc_dir, "rps_detach_audit_qc.py"),
-                }
-    rvt_jrn.execute_command(tab_name='Add-Ins',
-                            panel_name='  Revit Model Services (RMS)  ',
-                            command_module='RMSCmdExecutor',
-                            command_class='RMSCmdExecutorCommand',
-                            command_data=com_data,
-                            )
+    com_data = {
+        "SearchPaths": command_path,
+        "ModelName": model_path.name,
+        "OutputPath": log_dir,
+        "OutputPrefix": project_code,
+        "LogFile": log_dir / f"{time_stamp}_{command_name}_rms_exec_results.log",
+        "ScriptSource": command_path / "rps_detach_audit_qc.py",
+
+    }
+    rvt_jrn.execute_command(
+        tab_name='Add-Ins',
+        panel_name='  Revit Model Services (RMS)  ',
+        command_module='RMSCmdExecutor',
+        command_class='RMSCmdExecutorCommand',
+        command_data=com_data,
+    )
     rvt_jrn.close_model()
     rvt_jrn.write_journal(jrn_path)
 
 
-register = {"name": "qc_no_ws",
-            "post_process": {"func": bokeh_qc_graphs.update_graphs,
-                             "args": ["project_code", "html_path"]},
-            "rjm": cmd_journal,
-            }
+register = {
+    "name": "qc_no_ws",
+    "post_process": {"func": bokeh_qc_graphs.update_graphs,
+                     "args": ["project_code", "html_path"]},
+    "rjm": cmd_journal,
+}
